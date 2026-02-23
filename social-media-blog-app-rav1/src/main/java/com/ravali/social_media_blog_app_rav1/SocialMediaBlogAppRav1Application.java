8b8409aad1117e68f297b31672651be70b842ce9
RAVALI28 <ravalikavajinapally.rv@gmail.com>
2026-02-23 13:09:12 -0600

    Intial commit - SpringBoot Project Structure

---
+++ social-media-blog-app-rav1/src/main/java/com/ravali/social_media_blog_app_rav1/SocialMediaBlogAppRav1Application.java
@@ -0,0 +1,13 @@
+package com.ravali.social_media_blog_app_rav1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaBlogAppRav1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaBlogAppRav1Application.class, args);
+	}
+
+}
---
+++ social-media-blog-app-rav1/src/test/java/com/ravali/social_media_blog_app_rav1/SocialMediaBlogAppRav1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravali.social_media_blog_app_rav1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaBlogAppRav1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
